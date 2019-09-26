0f26df6e2c5e02e6a853f08e85520c12e3f2a44d
Matthias <m@tth.es>
2019-09-26 21:14:56 +0200

    fix startup scripts

--- MANUAL_SETUP.md
+++ MANUAL_SETUP.md
@@ -8,11 +8,30 @@ apt-get update && apt-get install -y docker.io curl git ccrypt pwgen
 docker swarm init
 
 pwgen 64 -s -1 | docker secret create rudl_cf_secret -
+```
 
-passwd=$(pwgen 512 -s -1) && echo $passwd | ccrypt | base64 -w 72 > cluster-secret.enc && echo $passwd | docker secret create rudl_principal_secret -
+### Create secrets and ssh key (One time setup)
 
+```bash
+passwd=$(pwgen 512 -s -1)
+echo $passwd | ccrypt | base64 -w 72 > rudl_principal_secret.enc
+ssh-keygen -t ed25519 -N "$passwd" -f rudl_principal_ssh_key
+```
+
+This will create a protected `rudl_principal_secret.enc` - file and a protected ssh_key.
+
+> You should commit these files with your repository!
+
+### Recover your cluster
 
 ```
+cat rudl_principal_secret.enc | base64 -d | ccrypt -d | docker secret create rudl_principal_secret -
+cat rudl_principal_ssh_key | docker secret create rudl_principal_ssh_key -
+```
+
+
+##
+
 
 
 ```
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,11 +5,40 @@ set -x -e
 mkdir -p /mnt/.ssh
 mkdir -p /mnt/log
 
+CONF_PRINCIPAL_SECRET_FILE=/run/secrets/rudl_principal_secret
+CONF_PRINCIPAL_SSH_KEY_FILE=/run/secrets/rudl_principal_ssh_key
+CONF_SSH_KEY=/mnt/.ssh/id_ed25519
 
-if [[ ! -f /mnt/.ssh/id_ed25519.pub ]]
+
+if [[ ! -f $CONF_PRINCIPAL_SECRET_FILE ]]
 then
-    echo "Creating new ssh key pair"
-    ssh-keygen -N "" -t ed25519 -f /mnt/.ssh/id_ed25519
+    echo "Error: rudl_principal_secret missing in $CONF_PRINCIPAL_SECRET_FILE"
+    echo "Can't unlock cluster"
+    echo "Make sure you created and added the secret by running 'docker secret create rudl_principal_secret <secretfile>'"
+
+    exit 2
+fi
+
+if [[ -f $CONF_PRINCIPAL_SSH_KEY_FILE ]]
+then
+    echo "Existing $CONF_PRINCIPAL_SSH_KEY_FILE -> Unlocking key"
+    cp $CONF_PRINCIPAL_SSH_KEY_FILE $CONF_SSH_KEY
+
+    echo "Unlocking key using principal_secret..."
+    ssh-keygen -p -P "$(cat $CONF_PRINCIPAL_SECRET_FILE)" -N "" -f $CONF_SSH_KEY
+
+    echo "Overwriting exising ssh key with key..."
+    ssh-keygen -y -f $CONF_SSH_KEY > "$CONF_SSH_KEY.pub"
+
+else
+    echo "No $CONF_PRINCIPAL_SSH_KEY_FILE found."
+fi;
+
+
+if [[ ! -f $CONF_SSH_KEY ]]
+then
+    echo "Creating new ssh key pair in $CONF_SSH_KEY..."
+    ssh-keygen -N "" -t ed25519 -f $CONF_SSH_KEY
 
 fi
 
